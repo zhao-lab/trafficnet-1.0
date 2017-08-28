c89b04e77be43754f3b69695960ea4f2223361af
Ding Zhao <zhaoding@umich.edu>
2017-08-28 10:27:43 -0400

    create table

--- import_tables.sql
+++ import_tables.sql
@@ -1,2 +1,5 @@
+create database if not exists SpOneDay;
+create database if not exists SpTwoMonth;
+
 LOAD DATA LOCAL INFILE '/home/jackjia/traffic/das1/DataWsu.csv' INTO TABLE DataWsu
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
