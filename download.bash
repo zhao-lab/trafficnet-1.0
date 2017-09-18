ab8568f45d3848fcc5a80e38ce7259813bae37c0
yaohuig <yaohuig@umich.edu>
2017-09-17 22:35:55 -0400

    bash script to download and move databases

---
+++ download.bash
@@ -0,0 +1,1 @@
+wget -i urlList.txt -nc
---
+++ zipAndMove.bash
@@ -0,0 +1,7 @@
+unzip \*
+mkdir ~/AWSData/DataDas; mv DataDas*.csv $_
+mkdir ~/AWSData/DataFrontTargets; mv DataFrontTargets*.csv $_
+mkdir ~/AWSData/DataGpsDas; mv DataGpsDas*.csv $_
+mkdir ~/AWSData/DataLane; mv DataLane*.csv $_
+mkdir ~/AWSData/DataWsu; mv DataWsu*.csv $_
+mkdir ~/AWSData/DAS1_Trip_Summary_File; mv mkdir ~/AWSData/ $_
