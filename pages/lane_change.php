3f0bc2b218226d1812fddf5535ce63037b0ebc76
yaohuig <yaohuig@umich.edu>
2017-10-17 16:35:42 -0400

    yaohui

--- pages/lane_change.php
+++ pages/lane_change.php
@@ -64,55 +64,10 @@
           </div>
         </nav>
       </header>
+
       <!-- Left side column. contains the logo and sidebar -->
-      <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-          <!-- search form -->
-          <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-              <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
-            </div>
-          </form>
-          <!-- /.search form -->
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu">
-            <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
-              <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
-              </a>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-files-o"></i>
-                <span>Scenario Store</span>
-                <span class="label label-primary pull-right">8</span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="free_flow.php"><i class="fa fa-circle-o"></i> Free Flow</a></li>
-                <li><a href="lane_departure.php"><i class="fa fa-circle-o"></i> Lane Departure</a></li>
-                <li><a href="lane_change.php"><i class="fa fa-circle-o"></i> Lane Change</a></li>
-                <li><a href="left_turn.php"><i class="fa fa-circle-o"></i> Left Turn</a></li>
-                <li><a href="pedestrain_crossing.php"><i class="fa fa-circle-o"></i> Pedestrain Crossing</a></li>
-                <li><a href="car_following.php"><i class="fa fa-circle-o"></i> Car Following</a></li>
-                <li><a href="intersection.php"><i class="fa fa-circle-o"></i> Intersection</a></li>
-                <li><a href="cyclist.php"><i class="fa fa-circle-o"></i> Cyclist</a></li>
-              </ul>
-            </li>
-            <li>
-              <a href="https://github.com/jyh0082007/TrafficNet">
-                <i class="fa fa-th"></i> <span>Contribute</span> <small class="label pull-right bg-green">new</small>
-              </a>
-            </li>
-          </ul>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
+      <?php include 'menubar.php';?>
+
 
       <!-- Right side column. Contains the navbar and content of the page -->
       <div class="content-wrapper">
