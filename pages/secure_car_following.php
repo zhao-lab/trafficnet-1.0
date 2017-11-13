7fd6b37d36e5193b9b7fd402975e1616a3b4bd46
yaohuig <yaohuig@umich.edu>
2017-11-13 11:41:08 -0500

    yaohui

---
+++ pages/secure_car_following.php
@@ -0,0 +1,22 @@
+<?php
+$user = $_POST['user'];
+$pass = $_POST['password'];
+
+if($user == "admin"
+&& $pass == "admin")
+{
+        include("car_following.php");
+}
+else
+{
+    if(isset($_POST))
+    {?>
+
+            <form method="POST" action="secure.php">
+            User <input type="text" name="user"></input><br/>
+            Pass <input type="password" name="pass"></input><br/>
+            <input type="submit" name="submit" value="Go"></input>
+            </form>
+    <?}
+}
+?>
