d58f7004b4f270cddcda1fe5fcb046fb17af550d
yaohuig <yaohuig@umich.edu>
2017-11-13 10:44:27 -0500

    yaohui

--- index.php
+++ index.php
@@ -57,10 +57,8 @@
           </div>
         </nav>
       </header>
-
       <!-- Left side column. contains the logo and sidebar -->
       <?php include 'menubar.php';?>
-
 
       <!-- Right side column. Contains the navbar and content of the page -->
       <div class="content-wrapper">
