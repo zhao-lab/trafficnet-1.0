9ee05dea23fd0a62a1123e6d452c19ae48699aa2
yaohuig <yaohuig@umich.edu>
2017-11-13 10:40:37 -0500

    yaohui

---
+++ menubar.php
@@ -0,0 +1,51 @@
+      <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- Sidebar user panel -->
+          <!-- search form -->
+          <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+              <input type="text" name="q" class="form-control" placeholder="Search..."/>
+              <span class="input-group-btn">
+                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+              </span>
+            </div>
+          </form>
+          <!-- /.search form -->
+          <!-- sidebar menu: : style can be found in sidebar.less -->
+          <ul class="sidebar-menu">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="active treeview">
+              <a href="http://traffic-net.org/index.html">
+                <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+              </a>
+            </li>
+            <li class="treeview">
+              <a href="http://traffic-net.org/index.html">
+                <i class="fa fa-files-o"></i>
+                <span>Scenario Store</span>
+                <span class="label label-primary pull-right">8</span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="free_flow.php"><i class="fa fa-circle-o"></i> Free Flow</a></li>
+                <!-- <li><a href="lane_departure.php"><i class="fa fa-circle-o"></i> Lane Departure</a></li> -->
+                <li><a href="lane_change.php"><i class="fa fa-circle-o"></i> Lane Change</a></li>
+                <!-- <li><a href="left_turn.php"><i class="fa fa-circle-o"></i> Left Turn</a></li> -->
+                <li><a href="pedestrain_crossing.php"><i class="fa fa-circle-o"></i> Pedestrain</a></li>
+                <li><a href="car_following.php"><i class="fa fa-circle-o"></i> Car Following</a></li>
+                <!-- <li><a href="intersection.php"><i class="fa fa-circle-o"></i> Intersection</a></li> -->
+                <li><a href="cyclist.php"><i class="fa fa-circle-o"></i> Cyclist</a></li>
+                <li><a href="primitiveOneDay.php"><i class="fa fa-circle-o"></i> Driving primitive</a></li>
+                <li><a href="v2v.php"><i class="fa fa-circle-o"></i> v2v</a></li>
+                <li><a href="cutInEvents.php"><i class="fa fa-circle-o"></i> Cut-in Events</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="https://github.com/TrafficNet">
+                <i class="fa fa-th"></i> <span>Github</span> <small class="label pull-right bg-green">new</small>
+              </a>
+            </li>
+          </ul>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
