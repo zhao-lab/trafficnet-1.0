1faed2069d67fe6e244ea5de8cfa899ff5bcf0d5
yaohuig <yaohuig@umich.edu>
2017-11-13 11:59:21 -0500

    yaohui

--- pages/secure_car_following.php
+++ pages/secure_car_following.php
@@ -12,7 +12,7 @@ else
     if(isset($_POST))
     {?>
 
-            <form method="POST" action="secure.php">
+            <form method="POST" action="secure_car_following.php">
             User <input type="text" name="user"></input><br/>
             Pass <input type="password" name="pass"></input><br/>
             <input type="submit" name="submit" value="Go"></input>
