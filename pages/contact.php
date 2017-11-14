d8d852714bf9ce57df59dca04f108cf96317a766
yaohuig <yaohuig@umich.edu>
2017-11-14 11:02:19 -0500

    yaohui

--- pages/contact.php
+++ pages/contact.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+   <title>TrafficNet | Contact information</title>
+   <link href="style/style.css" rel="stylesheet" type="text/css" />
+</head>
 <body>
 
 <h1>TrafficNet | Contact information</h1>
 
-      <div id="source">This dataset has been purchased by Mcity and only visible to Mcity members. </div>
-      <div id="source"> </div>
-
-      <div id="source">For further information, please contact <a href="https://mcity.umich.edu/">Mcity</a> at University of Michigan</div>
-      <div id="source"> </div>
+      <div id="source">This dataset has been purchased by the Mcity. <br>
+      For further information, please contact <a href="https://mcity.umich.edu/">Mcity</a> at University of Michigan</div>
       <div id="source"> </div>
 
 
